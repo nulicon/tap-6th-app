f1a4d99632836ac0ec5e16b825b98e1ce7b65633
nulicon <103513513+nulicon@users.noreply.github.com>
2023-10-20 15:35:11 +0800

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu! from Github";
+		return "Greetings from Spring Boot + Tanzu!";
 	}
 
 }
